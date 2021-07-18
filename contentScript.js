ddd42de14400d4ef3cb56487f7db260d0f3514b4
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2021-07-18 17:37:36 -0300

    it works!

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,16 @@
-let titleh1s = document.body.querySelectorAll(".nshelp_title")[0];
+const titleh1s = document.body.querySelectorAll(".nshelp_title")[0];
 
 let pgtitle = document.getElementsByTagName('title')[0];
 
 if(titleh1s && pgtitle) {
     pgtitle.innerText = titleh1s.innerText + ' - Netsuite Help Center';
 }
+
+const head = document.getElementsByTagName('head')[0];
+
+let mylinktag = document.createElement('link');
+mylinktag.rel = 'icon';
+mylinktag.href = chrome.runtime.getURL('/images/myfavicon.png');
+mylinktag.type = 'image/png';
+
+head.appendChild(mylinktag);
