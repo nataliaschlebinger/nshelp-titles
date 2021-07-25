78e6e086ce70a9edc016c8f729a35d9ffa8a50c6
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2021-07-25 16:47:08 -0300

    add click event to search input (had forgot)

--- contentScript.js
+++ contentScript.js
@@ -14,4 +14,13 @@ const searchInput = document.getElementById("searchfld");
 searchInput.addEventListener("keypress", function (e) {
     if (e.key == "Enter")
         document.title = 'Searched: ' + String(this.value).trim() + ' - Nesuite Help Center';
+});
+searchInput.addEventListener("click", function(e){
+    let offsetx = e.offsetX || (e.pageX - e.target.offsetLeft);
+    let searchInputWidth = this.offsetWidth;
+    // 32 = SEARCH_ICON_WIDTH
+    if(offsetx + 32 > searchInputWidth)
+    {
+        document.title = 'Searched: ' + String(this.value).trim() + ' - Nesuite Help Center';
+    }
 })
